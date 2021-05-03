f2c7b6c6a0b37ab6c312f1994dae85b360eb20fa
ZackaryHernandez <56282839+ZackaryHernandez@users.noreply.github.com>
2021-05-03 12:38:10 -0700

    Update Queries_NorthwindDB_DBeaver.sql

--- Scripts/Queries_NorthwindDB_DBeaver.sql
+++ Scripts/Queries_NorthwindDB_DBeaver.sql
@@ -7,7 +7,541 @@
 #Zack Hernandez
 #########################
 
-#45 
+#57 Done    
+#Customers with multiple orders in 5 day period, version 2
+#There’s another way of solving the problem above, using a Window function. 
+SELECT
+    CustomerID
+    ,DATE(OrderDate) as InitialOrderDate
+    ,DATE(LEAD(OrderDate,1)
+       OVER (PARTITION by CustomerID order by CustomerID, OrderDate)
+     ) as NextOrderDate
+    FROM Orders
+)SELECT
+	CustomerID
+	,InitialOrderDate
+	,NextOrderDate
+	,DateDiff (NextOrderDate, InitialOrderDate) as DaysBetweenOrders
+From NextOrderDate
+Where
+	DateDiff (NextOrderDate, InitialOrderDate ) <= 5;
+
+
+
+#56 Customers with multiple orders in 5 day period
+#There are some customers for whom freight is a major expense when ordering from Northwind.
+#However, by batching up their orders, and making one larger order instead of multiple smaller orders in a short period of time,
+# they could reduce their freight costs significantly.
+#Show those customers who have made more than 1 order in a 5 day period. 
+#The sales people will use this to help customers reduce their freight costs.
+#Note: There are more than one way of solving this kind of problem. For this problem, we will not be using Window functions.
+#Moral: When comparing items in the same table join on the same table with different alias'
+#and use the where claus to draw comparisons between Initial and Final Data. 
+Select
+	InitialOrder.CustomerID
+	,InitialOrder.OrderID as InitialOrderID 
+	,Date(InitialOrder.OrderDate) as InitialOrderDate 
+	,NextOrder.OrderID as NextOrderID 
+	,Date(NextOrder.OrderDate) as NextOrderDate 
+	,DateDiff(NextOrder.OrderDate, InitialOrder.OrderDate) as DaysBetweenOrders
+FROM Orders InitialOrder
+	JOIN Orders NextOrder
+		ON InitialOrder.CustomerID = NextOrder.CustomerID
+WHERE
+	InitialOrder.OrderID < NextOrder.OrderID
+	and
+	DateDiff(NextOrder.OrderDate, InitialOrder.OrderDate) <= 5
+Order by 
+	InitialOrder.CustomerID,
+	InitialOrder.OrderID;
+
+#55 First order in each country
+#Looking at the Orders table—we’d like to show details for each order 
+#that was the first in that particular country, ordered by OrderID.
+#So, for each country, we want one row. That row should contain the
+# earliest order for that country, with the associated
+# ShipCountry, CustomerID, OrderID, and OrderDate.
+
+#THREW IN THE TOWEL. Window functions to solve the problem.
+
+
+with OrdersByCountry as (
+    Select
+        ShipCountry
+        ,CustomerID
+        ,OrderID
+        ,Date(OrderDate) as OrderDate
+        ,Row_Number()
+            over (Partition by ShipCountry)
+            as RowNumberPerCountry
+    From Orders
+) Select
+    ShipCountry
+    ,CustomerID
+    ,OrderID
+    ,OrderDate
+From OrdersByCountry
+Where
+RowNumberPerCountry = 1 Order by
+    ShipCountry;
+
+
+#54 Countries with suppliers or customers, version 3
+# The output in the above practice problem is improved, but it’s still not ideal
+#What we’d really like to see is the country name, the total suppliers, and the total customers.
+# Moral: Alter distinct cte to a group by cte to get count of countries. 
+WITH AllCountries AS 
+		(SELECT Country FROM Suppliers
+		Union
+		SELECT Country FROM Customers)
+	,SupplierCountries AS
+		(SELECT 
+			Country,
+			COUNT(*) AS TotalSuppliers
+		 FROM Suppliers
+		 GROUP BY 
+		 	Country)
+	,CustomerCountries AS
+    	(SELECT 
+    		Country,
+    		COUNT(*) AS TotalCustomers
+    	 FROM Customers
+    	 GROUP BY Country)
+SELECT
+	AllCountries.Country AS AllCountries,
+	IFNULL(SupplierCountries.TotalSuppliers, 0) AS TotalSuppliers,
+	IFNULL(CustomerCountries.TotalCustomers, 0) AS TotalCustomers
+FROM AllCountries 
+	Left Join CustomerCountries
+		ON AllCountries.Country = CustomerCountries.Country
+	Left Join SupplierCountries
+		ON AllCountries.Country = SupplierCountries.Country
+Order by AllCountries.Country;
+
+#53 Countries with suppliers or customers, version 2
+# The employees going on the business trip don’t want just a raw list of countries, they want more details. 
+# We’d like to see output like the below, in the Expected Results.
+#Begin by creating a country CTE that is a union of both country columns. 
+#Then make separate CTEs that represent distinct supplier countries and distinct customer countries/
+#Join these distinct CTEs together on the unioned column. This works because the union column has all possible countries.
+#We can now select colums from AllCountries (unioned column) and the other two distinct columns. 
+
+WITH AllCountries AS 
+		(SELECT Country FROM Suppliers
+		Union
+		SELECT Country FROM Customers)
+	,SupplierCountries AS
+		(SELECT DISTINCT Country FROM Suppliers)
+	,CustomerCountries AS
+    	(SELECT DISTINCT Country FROM Customers)
+SELECT
+	AllCountries.Country AS AllCountries,
+	SupplierCountries.Country AS SupplierCountry,
+	CustomerCountries.Country AS CustomerCountry
+FROM AllCountries 
+	Left Join CustomerCountries
+		ON AllCountries.Country = CustomerCountries.Country
+	Left Join SupplierCountries
+		ON AllCountries.Country = SupplierCountries.Country
+Order by AllCountries.Country;
+
+
+	
+
+#52 Countries with suppliers or customers
+#Some Northwind employees are planning a business trip, and would like to visit as many suppliers and customers as possible. 
+#Moral of the story: UNION to combine two columns together with distinct values.
+#UNION ALL for non-distinct values included (all values)
+SELECT 	
+	Country
+FROM Customers c  
+Union
+SELECT 
+	Country
+FROM Suppliers s 
+ORDER BY Country;
+
+
+
+#51. Customer grouping—flexible
+#Andrew, the VP of Sales is still thinking about how best to group customers, 
+#and define low, medium, high, and very high value customers.
+# He now wants complete flexibility in grouping the customers,
+# based on the dollar amount they've ordered. 
+#He doesn’t want to have to edit SQL in order to change the boundaries of the customer groups.
+#There's a table called CustomerGroupThreshold that you will need to use. Use only orders from 2016.
+
+#Analysis: The key question I asked myself is how am I going to join CustomerGroupThresholds table
+#on to Orders2016 CTE when there is no matching primary key?
+#the answer is to join Orders2016.TotalOrderAmount on 2 fields that are represent an upper and lower limit. 
+# Range bottom and range top. 
+SELECT *
+FROM CustomerGroupThresholds cgt 
+
+WITH Orders2016 AS (
+SELECT
+	c.CustomerID,
+	c.CompanyName,
+	SUM(Quantity * UnitPrice) as TotalOrderAmount
+FROM Customers c
+	JOIN Orders o
+		ON c.CustomerID = o.CustomerID 
+	JOIN OrderDetails od 
+		ON o.OrderID = od.OrderID 
+WHERE OrderDate >= '2016-01-01' and OrderDate < '2017-01-01'
+GROUP BY 
+	c.CustomerID,
+	c.CompanyName
+)
+SELECT 
+	CustomerID,
+    CompanyName,
+    TotalOrderAmount,
+    CustomerGroupName
+FROM Orders2016
+	JOIN CustomerGroupThresholds
+		ON Orders2016.TotalOrderAmount BETWEEN 
+		CustomerGroupThresholds.RangeBottom 
+		AND 
+		CustomerGroupThresholds.RangeTop 
+ORDER BY 
+	CustomerID;
+	
+
+
+
+#50 Customer grouping with percentage
+#Based on the above query, show all the defined CustomerGroups, and the percentage in each. 
+#Sort by the total in each group, in descending order.
+#added an intermediate CTE called CustomerGrouping.
+#CustomerGrouping is referenced twice—once to get the total number of customers in the specific group, 
+#and once to get the total, as the denominator for the percentage.
+with Orders2016 as (
+    Select
+		Customers.CustomerID
+		,Customers.CompanyName
+		,SUM(Quantity * UnitPrice) as TotalOrderAmount
+    From Customers
+        Join Orders
+			on Orders.CustomerID = Customers.CustomerID 
+		Join OrderDetails
+			on Orders.OrderID = OrderDetails.OrderID
+Where OrderDate >= '2016-01-01' and OrderDate < '2017-01-01'
+Group by 
+	Customers.CustomerID,
+	Customers.CompanyName
+),CustomerGrouping as (
+    Select
+        CustomerID
+        ,CompanyName
+        ,TotalOrderAmount
+        ,Case
+			when TotalOrderAmount >= 0 and TotalOrderAmount < 1000 then 'Low'
+			when TotalOrderAmount >= 1000 and TotalOrderAmount < 5000 then 'Medium'
+			when TotalOrderAmount >= 5000 and TotalOrderAmount <10000 then 'High'
+			when TotalOrderAmount >= 10000 then 'Very High'
+        End
+            as CustomerGroup
+    from Orders2016
+)
+Select
+    CustomerGroup
+    ,Count(*) as TotalInGroup
+    ,Count(*)/(select count(*) from CustomerGrouping)
+        as PercentageInGroup
+from CustomerGrouping
+group by CustomerGroup
+order by TotalInGroup  desc;
+
+
+
+
+#49 Customer grouping—fix null
+#Fix the null value. Since TotalOrderAmount is a decimal, use comparison operators. 
+#Using “between” would have been fine for integer values, but not for decimal.
+with Orders2016 as (
+    Select
+		Customers.CustomerID
+		,Customers.CompanyName
+		,SUM(Quantity * UnitPrice) as TotalOrderAmount
+    From Customers
+        Join Orders
+			on Orders.CustomerID = Customers.CustomerID 
+		Join OrderDetails
+			on Orders.OrderID = OrderDetails.OrderID
+Where OrderDate >= '2016-01-01' and OrderDate < '2017-01-01'
+Group by 
+	Customers.CustomerID,
+	Customers.CompanyName
+) Select
+    CustomerID
+    ,CompanyName
+    ,TotalOrderAmount
+    ,Case
+        when TotalOrderAmount >= 0 and TotalOrderAmount < 1000 then 'Low'
+		when TotalOrderAmount >= 1000 and TotalOrderAmount < 5000 then 'Medium'
+		when TotalOrderAmount >= 5000 and TotalOrderAmount < 10000 then 'High'
+		when TotalOrderAmount >= 10000 then 'Very High'
+    End
+        as CustomerGroup
+from Orders2016
+Order by CustomerID;
+
+
+
+#48
+#Customer grouping
+#Andrew Fuller, the VP of sales at Northwind, would like to do a sales campaign for existing customers. 
+#He'd like to categorize customers into groups, based on how much they ordered in 2016. 
+#Then, depending on which group the customer is in, he will target the customer with different sales materials.
+#The customer grouping categories are 0 to 1,000, 1,000 to 5,000, 5,000 to 10,000, and over 10,000. 
+#So, if the total dollar amount of the customer’s purchases in that year were between 0 to 1,000, they would be in the “Low” group. 
+#A customer with purchase from 1,000 to 5,000 would be in the “Medium” group, and so on.
+#A good starting point for this query is the answer from the problem “High-value customers— total orders”. Also, we only want to show customers who have ordered in 2016.
+# Order the results by CustomerID.
+
+# my answer
+SELECT c.CustomerID,
+       c.CompanyName, 
+       SUM(Quantity * UnitPrice) AS TotalOrderAmount,
+       CASE
+       	WHEN SUM(Quantity * UnitPrice) < 1000 THEN 'LOW'
+       	WHEN SUM(Quantity * UnitPrice) >= 1000 AND SUM(Quantity * UnitPrice) < 5000 THEN 'MEDIUM'
+       	WHEN SUM(Quantity * UnitPrice) >= 5000 AND SUM(Quantity * UnitPrice) < 10000 THEN 'HIGH'
+       	ELSE 'EXTREMELY HIGH'
+       	END AS CustomerGroup
+FROM Customers c
+    JOIN Orders o
+		ON o.CustomerID = c.CustomerID 
+	JOIN OrderDetails od
+		ON o.OrderID = od.OrderID
+WHERE
+OrderDate >= '2016-01-01' and OrderDate < '2017-01-01'
+GROUP BY 
+	c.CustomerID,
+	c.CompanyName 
+ORDER BY CustomerID;
+
+#Sylvias answer to 48:
+#the benefit of using a CTE here is to not repeat the SUM(Quantity * UnitPrice) calculation.
+# notice in my query I repeated it 5 times. IN Sylvia's query it is only done once. 
+# this plays into the concept of DRY: don't repeat yourself. 
+
+with Orders2016 as (
+    Select
+		Customers.CustomerID
+		,Customers.CompanyName
+		,SUM(Quantity * UnitPrice) as TotalOrderAmount
+    From Customers
+        Join Orders
+			on Orders.CustomerID = Customers.CustomerID 
+		Join OrderDetails
+			on Orders.OrderID = OrderDetails.OrderID
+Where OrderDate >= '2016-01-01' and OrderDate < '2017-01-01'
+Group by 
+	Customers.CustomerID,
+	Customers.CompanyName
+) Select
+    CustomerID
+    ,CompanyName
+    ,TotalOrderAmount
+    ,Case
+        when TotalOrderAmount between 0 and 1000 then 'Low'
+        when TotalOrderAmount between 1001 and 5001 then 'Medium'
+        when TotalOrderAmount between 5001 and 10000 then 'High'
+        when TotalOrderAmount >= 10000 then 'Very High'
+    End
+        as CustomerGroup
+from Orders2016
+Order by CustomerID;
+
+
+
+  
+
+
+#47 Late orders vs. total orders—fix decimaL
+#You could also use format below in place of cast. 
+#FORMAT(IFNULL(LateOrders.TotalOrders, 0) / AllOrders.TotalOrders, 2) AS PercentLateOrders
+WITH LateOrders AS (
+    SELECT
+        EmployeeID,
+        Count(*) AS TotalOrders
+    FROM Orders
+    WHERE RequiredDate <= ShippedDate 
+    GROUP BY 
+		  EmployeeID 
+	),
+AllOrders AS ( 
+	SELECT
+        EmployeeID,
+        Count(*) AS TotalOrders
+    FROM Orders
+    GROUP BY
+        EmployeeID
+    )
+SELECT
+	Employees.EmployeeID,
+	LastName,
+	AllOrders.TotalOrders AS AllOrders, 
+	IFNULL(LateOrders.TotalOrders, 0) AS LateOrders,
+	CAST(
+		IFNULL(LateOrders.TotalOrders, 0) / AllOrders.TotalOrders 
+		AS Decimal(4, 2) )
+		AS PercentLateOrders
+FROM Employees
+    LEFT JOIN AllOrders
+		ON AllOrders.EmployeeID = Employees.EmployeeID 
+	LEFT JOIN LateOrders
+		ON LateOrders.EmployeeID = Employees.EmployeeID 
+ORDER BY 
+	Employees.EmployeeID;
+
+#46Late orders vs. total orders—percentage
+#Now we want to get the percentage of late orders over total orders.
+#In some other database systems (such as SQL Server), you would need to explicitly convert one of the 
+#fields to a decimal datatype, or implicitly convert them by multiplying by 1.0 in 
+#order to get a decimal output.
+WITH LateOrders AS (
+    SELECT
+        EmployeeID,
+        Count(*) AS TotalOrders
+    FROM Orders
+    WHERE RequiredDate <= ShippedDate 
+    GROUP BY 
+		  EmployeeID 
+	),
+AllOrders AS ( 
+	SELECT
+        EmployeeID,
+        Count(*) AS TotalOrders
+    FROM Orders
+    GROUP BY
+        EmployeeID
+    )
+SELECT
+	Employees.EmployeeID,
+	LastName,
+	AllOrders.TotalOrders AS AllOrders, 
+	IFNULL(LateOrders.TotalOrders, 0) AS LateOrders,
+	IFNULL(LateOrders.TotalOrders, 0) / AllOrders.TotalOrders AS PercentLateOrders
+FROM Employees
+    LEFT JOIN AllOrders
+		ON AllOrders.EmployeeID = Employees.EmployeeID 
+	LEFT JOIN LateOrders
+		ON LateOrders.EmployeeID = Employees.EmployeeID 
+ORDER BY 
+	Employees.EmployeeID;
+
+#45
+#Late orders vs. total orders—fix null
+#Continuing on the answer for above query, let's fix the results for row 5 - Buchanan. 
+#He should have a 0 instead of a Null in LateOrders.
+#Moral use ifnull in the final select, not the CTE
+WITH LateOrders AS (
+    SELECT
+        EmployeeID,
+        Count(*) AS TotalOrders
+    FROM Orders
+    WHERE RequiredDate <= ShippedDate 
+    GROUP BY 
+		  EmployeeID 
+	),
+AllOrders AS ( 
+	SELECT
+        EmployeeID,
+        Count(*) AS TotalOrders
+    FROM Orders
+    GROUP BY
+        EmployeeID
+    )
+SELECT
+	Employees.EmployeeID,
+	LastName,
+	AllOrders.TotalOrders AS AllOrders, 
+	IFNULL(LateOrders.TotalOrders, 0) AS LateOrders
+FROM Employees
+    LEFT JOIN AllOrders
+		ON AllOrders.EmployeeID = Employees.EmployeeID 
+	LEFT JOIN LateOrders
+		ON LateOrders.EmployeeID = Employees.EmployeeID 
+ORDER BY 
+	Employees.EmployeeID;
+
+
+#####Resume Sylvias course
+SELECT o.OrderID,
+	   od.UnitPrice
+FROM Orders o 
+JOIN OrderDetails od
+	ON o.OrderID = od.OrderID 
+
+SELECT 
+	o.OrderID,
+	o.EmployeeID,
+	CASE WHEN YEAR(OrderDate) = '2016' THEN SUM(od.UnitPrice) ELSE 0 END AS '2016_total',
+	CASE WHEN YEAR(OrderDate) = '2015' THEN SUM(od.UnitPrice) ELSE 0 END AS '2015_total',
+	CASE WHEN YEAR(OrderDate) = '2014' THEN SUM(od.UnitPrice) ELSE 0 END AS '2014_total'
+FROM Orders o 
+JOIN OrderDetails od 
+	ON o.OrderID = od.OrderID
+GROUP BY 
+	o.OrderID,
+	o.EmployeeID,
+ 	o.OrderDate;
+
+
+
+
+#Break from Sylvia's Course
+#Task: Daniel Fields provided a SQL Server query. Interpret this query:
+#My interpretation:
+#The first column [Vendor] containts a concatenated string that begins with v.vendor_name, then a single space, and then ends with vendor_id as a varchar of length 10 datatype. This is all one column. 
+#Case statements 2021 down to 2013 function like this: If column year_for_period contains 'xxyy' then sum all corresponding values in the purchase_amount column. Else, (if the year is not 'xxyy', then 0.
+#
+
+
+
+
+Select 
+	v.vendor_name + ' (' + CAST(J.vendor_id as varchar(10)) + ')' as [Vendor],
+	CASE WHEN year_for_period = '2021' THEN sum(purchase_amount) ELSE 0 END as [2021],
+	CASE WHEN year_for_period = '2020' THEN sum(purchase_amount) ELSE 0 END as [2020],
+	CASE WHEN year_for_period = '2019' THEN sum(purchase_amount)  ELSE 0 END as [2019],
+	CASE WHEN year_for_period = '2018' THEN sum(purchase_amount)  ELSE 0 END as [2018],
+	CASE WHEN year_for_period = '2017' THEN sum(purchase_amount)  ELSE 0 END as [2017],
+	CASE WHEN year_for_period = '2016' THEN sum(purchase_amount)  ELSE 0 END as [2016],
+	CASE WHEN year_for_period = '2015' THEN sum(purchase_amount)  ELSE 0 END as [2015],
+	CASE WHEN year_for_period = '2014' THEN sum(purchase_amount)  ELSE 0 END as [2014],
+	CASE WHEN year_for_period = '2013' THEN sum(purchase_amount)  ELSE 0 END as [2013]
+	INTO #TEMP
+from p21_view_purchases_journal as J
+	inner join vendor as v 
+		on J.vendor_id = v.vendor_id
+where J.vendor_id IN (@Vendor)
+group by J.vendor_id, year_for_period, v.vendor_name
+
+ 
+
+Select
+	[Vendor],
+	SUM([2021]) as [2021],
+	SUM([2020]) as [2020],
+	SUM([2019]) as [2019],
+	SUM([2018]) as [2018],
+	SUM([2017]) as [2017],
+	SUM([2016]) as [2016],	
+	SUM([2015]) as [2015],
+	SUM([2014]) as [2014],
+	SUM([2013]) as [2013]
+from #TEMP
+GROUP BY [Vendor]
+Order by [Vendor] asc
+
+ 
+
+drop table #TEMP
+
 
 #44 Late Orders vs. Total Orders -- missing employee
 #There's an employtee missing in the answer from the problem above. Fix the SQL to show all
